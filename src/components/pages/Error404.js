0e58dadf7ac56529eeb8b9ccd30a84d23512830a
maxicordoba <maxi_cordoba32@hotmail.com>
2021-12-10 23:47:17 -0300

    se creo la pagina del error 404

--- src/components/pages/Error404.js
+++ src/components/pages/Error404.js
@@ -5,7 +5,7 @@ import error from '../../img/error-404-2.jpg';
 const Error404 = () => {
         return (
             <Container className='text-center'>
-                <img src={error} alt="Error 404" className='w-100' />
+                <img src={error} alt="imagen error 404" className='w-100' />
             </Container>
             
         );
