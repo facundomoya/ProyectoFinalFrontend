f05d45c37be615ba2d2c5c9fff71f6384383a36f
maxicordoba <maxi_cordoba32@hotmail.com>
2021-12-10 23:43:46 -0300

    se crearon los componentes Navbar y Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from './components/common/Navigation'
+import Footer from './components/common/Footer'
+import Error404 from './components/pages/Error404'
+import Inicio from './components/pages/Inicio';
 
 function App() {
   return (
-    <div className="">
-      
-    </div>
+    <Router>
+      <Navigation/>
+      <Routes>
+      <Route exact path='/' element={<Inicio></Inicio>}></Route>
+        <Route exact path='*' element={<Error404></Error404>}></Route>
+      </Routes>
+      <Footer></Footer>
+    </Router>
   );
 }
 
